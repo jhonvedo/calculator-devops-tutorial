7a348f2a8386a138962334218e7b0b0b56aa30bd
Jhon Acevedo <developer@jhonacevedo.com>
2024-10-27 22:51:37 -0500

    Updates

---
+++ precache-manifest.c3814cc5976f39ecfb6341cf76287bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de1faa20b16417923243e67614fd680",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "cbae5690f72293ab035f",
+    "url": "/calculator/static/css/main.37131298.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "cbae5690f72293ab035f",
+    "url": "/calculator/static/js/main.037f25d8.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
